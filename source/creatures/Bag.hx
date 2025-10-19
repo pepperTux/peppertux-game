42a43a2646a8740f6fac096d4ade7c459e160137
Vaesea <159406785+Vaesea@users.noreply.github.com>
2025-10-19 15:32:58 +0100

    Oops

--- source/creatures/Bag.hx
+++ source/creatures/Bag.hx
@@ -4,7 +4,7 @@ import flixel.graphics.frames.FlxAtlasFrames;
 
 class Bag extends Enemy
 {
-    var bounceHeight = 512;
+    var bounceHeight = 512; // Jumping height
     var bagImage = FlxAtlasFrames.fromSparrow("assets/images/characters/bag.png", "assets/images/characters/bag.xml");
 
     public function new(x:Float, y:Float)
@@ -13,16 +13,13 @@ class Bag extends Enemy
 
         frames = bagImage;
         animation.addByPrefix('jump', 'jump', 8, false);
-
-        immovable = false;
+        
         acceleration.y = gravity;
-
-        alive = true;
     }
 
     override private function move()
     {
-        if (isTouching(FLOOR))
+        if (isTouching(FLOOR)) // When touching floor, jump!
         {
             velocity.y = -bounceHeight;
             animation.play('jump');
